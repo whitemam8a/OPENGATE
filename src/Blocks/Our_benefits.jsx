6ccc4eb7cf2ad8a8f5644772eee0db75b98b7999
Daniil <den.sereda16@gmail.com>
2024-12-15 16:04:37 +0100

    Edits to the benefits block

--- src/Blocks/Our_benefits.jsx
+++ src/Blocks/Our_benefits.jsx
@@ -7,26 +7,30 @@ const Our_benefits = ({ title }) => {
       <div>
         <h2 className="text-center my-10">{title}</h2>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 ">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 ">
         <Card
+          image={"/pictures/1pic_benefits.png"}
           title={"Simple and easy to work with"}
           description={
             "Regardless of your experience with digital, we can help you create and sell NFT with ease, removing any technical barriers."
           }
         />
         <Card
+          image={"/pictures/2pic_benefits.png"}
           title={"Promoting your unique brand"}
           description={
             "We can help strategize and increase the visibility of your art to bring more attention to your work."
           }
         />
         <Card
+          image={"/pictures/3pic_benefits.png"}
           title={"Entering the international market"}
           description={
             "We provide access to global NFT platforms where your work can find new fans and buyers around the world."
           }
         />
         <Card
+          image={"/pictures/4pic_benefits.png"}
           title={"New financial opportunities"}
           description={
             "NFT will open up new revenue channels for you, allowing you to monetize your creativity on new levels."
--- src/Components/Card_Benefits.jsx
+++ src/Components/Card_Benefits.jsx
@@ -2,18 +2,19 @@ import React from "react";
 
 const Card = ({ image, altImage, title, description }) => {
   return (
-    <div className="card bg-slate-50 mx-3 shadow-xl">
+    <div className="card bg-slate-50 mx-3 shadow-xl shadow-[#9F1E6E]">
       <figure className="flex justify-center items-center h-48 w-full overflow-hidden">
-        <img src={image} alt={altImage} />
+        <img
+          src={image}
+          alt={altImage}
+          className="h-full w-full object-cover"
+        />
       </figure>
       <div className="card-body">
         <h3 className="card-title text-xl font-semibold text-gray-800">
           {title}
         </h3>
         <p className="text-gray-600 my-2">{description}</p>
-        {/* <div className="card-actions">
-          <strong className="text-lg text-gray-950">{`${price}€`}</strong>
-        </div> */}
       </div>
     </div>
     // <div className="transition-transform transform hover:scale-105 hover:shadow-xl bg-white rounded-lg overflow-hidden shadow-md hover:shadow-indigo-300 p-5">
