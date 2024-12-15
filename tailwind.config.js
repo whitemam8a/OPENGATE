81296a0ea7b8ff10a9f1c8df4711743b84b6b578
Daniil <den.sereda16@gmail.com>
2024-12-15 13:11:52 +0100

    fixed bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,7 @@ import daisyui from "daisyui";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      spacing: {
-        color: black,
-      },
-    },
+    extend: {},
   },
   plugins: [daisyui],
 };
